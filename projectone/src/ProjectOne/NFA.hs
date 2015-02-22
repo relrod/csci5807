a73fa827975fe90f71c68dbed8da4b45af39bb09
Ricky Elrod <ricky@elrod.me>
2015-02-22 18:26:28 -0500

    Start on NFA construction, missing or and concat
    Signed-off-by: Ricky Elrod <ricky@elrod.me>

--- projectone/src/ProjectOne/NFA.hs
+++ projectone/src/ProjectOne/NFA.hs
@@ -12,15 +12,17 @@
 module ProjectOne.NFA (
   NFA (..)
 , Edge (..)
+, fromRegex
 ) where
 
-import Data.Set
+import qualified Data.Set as S
+import qualified ProjectOne.RegexRule as R
 
 -- | Here we represent a nondeterministic finite automata (NFA).
-data NFA a = NFA { states    :: Set a
-                 , edges     :: Set (Edge a)
+data NFA a = NFA { states    :: S.Set a
+                 , edges     :: S.Set (Edge a)
                  , initial   :: a
-                 , accepting :: Set a
+                 , accepting :: S.Set a
                  } deriving (Eq, Ord, Show)
 
 -- | This type defines edges that appear in our graphs. An edge can either be a
@@ -30,3 +32,30 @@ data NFA a = NFA { states    :: Set a
 data Edge a = Edge a Char a
             | Epsilon a a
             deriving (Eq, Ord, Show)
+
+alterEdge :: Int -> Edge Int -> Edge Int
+alterEdge n (Edge x c y) = Edge (n + x) c (n + y)
+alterEdge n (Epsilon x y) = Epsilon (n + x) (n + y)
+
+fromRegex :: R.RegexRule -> NFA Int
+fromRegex (R.Literal c) = NFA
+                          (S.fromList [0, 1])
+                          (S.singleton $ Edge 0 c 1)
+                          0
+                          (S.singleton 1)
+fromRegex R.Epsilon = NFA
+                      (S.fromList [0, 1])
+                      (S.singleton $ Epsilon 0 1)
+                      0
+                      (S.singleton 1)
+fromRegex (R.Star r) = f . fromRegex $ r
+  where
+    f (NFA s e _ _) = NFA
+                      (S.map (+1) s `S.union` S.fromList [0, S.size s + 1])
+                      (S.map (alterEdge 1) e `S.union` S.fromList [ Epsilon 0 1
+                                                                  , Epsilon (S.size s) 1
+                                                                  , Epsilon 0 (S.size s + 1)
+                                                                  , Epsilon (S.size s) (S.size s + 1)
+                                                                  ])
+                      0
+                      (S.singleton (S.size s + 1))
